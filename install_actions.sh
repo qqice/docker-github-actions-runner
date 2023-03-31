b35b798722f9631209f186498277b1bbba35df66
qqice <39629131+qqice@users.noreply.github.com>
2023-03-31 15:17:45 +0800

    Update install_actions.sh

--- install_actions.sh
+++ install_actions.sh
@@ -6,7 +6,7 @@ export TARGET_ARCH="x64"
 if [[ $TARGETPLATFORM == "linux/arm64" ]]; then
   export TARGET_ARCH="arm64"
 fi
-curl -x socks5://127.0.0.1:7890 -L "https://github.com/actions/runner/releases/download/v${GH_RUNNER_VERSION}/actions-runner-linux-${TARGET_ARCH}-${GH_RUNNER_VERSION}.tar.gz" > actions.tar.gz
+curl -L "https://github.com/actions/runner/releases/download/v${GH_RUNNER_VERSION}/actions-runner-linux-${TARGET_ARCH}-${GH_RUNNER_VERSION}.tar.gz" > actions.tar.gz
 tar -zxf actions.tar.gz
 rm -f actions.tar.gz
 ./bin/installdependencies.sh
